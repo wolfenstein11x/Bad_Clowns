2e67104a6e85e463fdd805985ede215dd216b207
Charles Wolfe <c.wolfe132@gmail.com>
2021-12-30 12:45:29 -0700

    added first person controller

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 #pragma warning disable 618
 namespace UnityStandardAssets.Utility
@@ -8,18 +10,17 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-        public GUIText camSwitchButton;
+        //public  camSwitchButton;
         public GameObject[] objects;
-
 
         private int m_CurrentActiveObject;
 
-
+        
         private void OnEnable()
         {
             // active object starts from first in array
             m_CurrentActiveObject = 0;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+            //camSwitchButton = objects[m_CurrentActiveObject].name;
         }
 
 
@@ -33,7 +34,7 @@ namespace UnityStandardAssets.Utility
             }
 
             m_CurrentActiveObject = nextactiveobject;
-            camSwitchButton.text = objects[m_CurrentActiveObject].name;
+            //camSwitchButton = objects[m_CurrentActiveObject].name;
         }
     }
 }
