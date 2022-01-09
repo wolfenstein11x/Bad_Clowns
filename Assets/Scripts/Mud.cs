e209e37cf68f9b8bf8f2466a249e4f79f41375e9
Charles Wolfe <c.wolfe132@gmail.com>
2022-01-09 14:50:16 -0700

    added mud obstacle

---
+++ Assets/Scripts/Mud.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Mud : MonoBehaviour
+{
+    [SerializeField] float slowDownFactor = 0.1f;
+
+    RigidbodyFirstPersonController fpsController;
+    float forwardSpeedOriginal;
+    float backwardSpeedOriginal;
+    float strafeSpeedOriginal;
+
+    void Start()
+    {
+        fpsController = FindObjectOfType<RigidbodyFirstPersonController>();
+        forwardSpeedOriginal = fpsController.movementSettings.ForwardSpeed;
+        backwardSpeedOriginal = fpsController.movementSettings.BackwardSpeed;
+        strafeSpeedOriginal = fpsController.movementSettings.StrafeSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            fpsController.movementSettings.ForwardSpeed *= slowDownFactor;
+            fpsController.movementSettings.BackwardSpeed *= slowDownFactor;
+            fpsController.movementSettings.StrafeSpeed *= slowDownFactor;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            fpsController.movementSettings.ForwardSpeed *= (1 / slowDownFactor);
+            fpsController.movementSettings.BackwardSpeed *= (1 / slowDownFactor);
+            fpsController.movementSettings.StrafeSpeed *= (1 / slowDownFactor);
+        }
+    }
+}
