2f00f87610a4f2b8ceec3c3d7bd86fe3e0c80b7d
Charles Wolfe <c.wolfe132@gmail.com>
2022-01-26 21:13:43 -0700

    laughing clown spawns clown when it laughs

--- Assets/Scripts/LaughingClown.cs
+++ Assets/Scripts/LaughingClown.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class LaughingClown : MonoBehaviour
 {
+    [SerializeField] GameObject[] spawners;
+
+    [SerializeField] float spellRange = 15f;
     [SerializeField] float waitTimeMin = 3f;
     [SerializeField] float waitTimeMax = 7f;
+    [SerializeField] Transform spawnPoint;
 
     Animator animator;
     bool isLaughing;
+    float distanceToTarget = Mathf.Infinity;
+    Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         isLaughing = false;
-        animator = GetComponent<Animator>();        
+        animator = GetComponent<Animator>();     
+        target = FindObjectOfType<PlayerHealth>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isLaughing)
+        if (!isLaughing && TargetInRange())
         {
             StartCoroutine(LaughSpell());
         }
@@ -32,9 +39,25 @@ public class LaughingClown : MonoBehaviour
 
         animator.SetTrigger("laugh");
         isLaughing = true;
-
+        
         yield return new WaitForSeconds(waitTime);
         isLaughing = false;
+    }
+
+    public void SpawnClown()
+    {
+        var spawnerIndex = Random.Range(0, spawners.Length);
+
+        GameObject newClown = Instantiate(spawners[spawnerIndex], spawnPoint.position, spawnPoint.rotation) as GameObject;
+
+        newClown.transform.parent = transform;
+    }
+
+    private bool TargetInRange()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        return (distanceToTarget <= spellRange);
     }
 
 
