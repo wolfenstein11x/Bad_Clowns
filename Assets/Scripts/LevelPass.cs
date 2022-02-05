fe80fa7752094bb5ac004d22f4a81263e06cfaf7
Charles Wolfe <c.wolfe132@gmail.com>
2022-02-04 19:18:49 -0700

    added lamps and level passes, ready to build

---
+++ Assets/Scripts/LevelPass.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelPass : MonoBehaviour
+{
+    // TODO make player enter password to skip levels
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level_2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("BossBattle1");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level_4");
+    }
+
+
+}
